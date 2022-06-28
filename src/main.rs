c180f3049920c7787703f98ac7aacdd1ef10a23a
henrimeng_predilife <henri.meng@predilife.com>
2022-06-28 12:03:11 +0200

    [ADD] tcpStream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::Write;
+use std::net::TcpStream;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut stream = TcpStream::connect("localhost:7878");
+    match stream {
+        Ok(mut stream) => {
+            let message= "Hello".as_bytes();
+            let response = stream.write_all(&message);
+        }
+        Err(err) => panic!("Cannot connect : {err}"),
+    }
 }
