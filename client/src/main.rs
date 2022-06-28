2afe8fa9f58a07c5aadd4f99b252b4b36fd2a5f8
Ahmedine <krapulxx@gmail.com>
2022-06-28 12:38:13 +0200

    [ADD] package server / client / shared
    Commande pour run -> cargo run --bin package  
    $ cargo run --bin shared
    $ cargo run --bin server
    $ cargo run --bin client

---
+++ client/src/main.rs
@@ -0,0 +1,15 @@
+use std::io::Write;
+use std::net::TcpStream;
+
+fn main() {
+    println!("Hello, Client!");
+
+    let stream = TcpStream::connect("localhost:7878");
+    match stream {
+        Ok(mut stream) => {
+            let message = "Hello".as_bytes();
+            let _response = stream.write_all(&message);
+        }
+        Err(err) => panic!("Cannot connect : {err}"),
+    }
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,13 +2,13 @@ use std::io::Write;
 use std::net::TcpStream;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Server!");
 
-    let mut stream = TcpStream::connect("localhost:7878");
+    let stream = TcpStream::connect("localhost:7878");
     match stream {
         Ok(mut stream) => {
             let message= "Hello".as_bytes();
-            let response = stream.write_all(&message);
+            let _response = stream.write_all(&message);
         }
         Err(err) => panic!("Cannot connect : {err}"),
     }
