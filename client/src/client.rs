e877bbe4444e1ed1436a7272d2992c872226b096
henrimeng_predilife <henri.meng@predilife.com>
2022-07-03 00:29:29 +0200

    [REFACTO] extract from send : read_message_size, read_message_content functions

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,4 +1,5 @@
 use std::char::from_u32;
+use std::fmt::Error;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
@@ -14,24 +15,33 @@ impl Client {
         let msg_len = &(msg.len() as u32).to_be_bytes();
         self.stream.write(msg_len).unwrap();
         self.stream.write(msg.as_bytes()).unwrap();
+
         println!("\t> Sent {}", msg);
 
+        let message_size = self.read_message_size().unwrap();
+        self.read_message_content(message_size as usize);
+    }
+
+    fn read_message_size(&mut self) -> Result<u32, std::io::Error> {
         let mut buffer = [0 as u8; 4];
-        match self.stream.read_exact(&mut buffer) {
+        return match self.stream.read_exact(&mut buffer) {
             Ok(_) => {
                 let message_size = u32::from_be_bytes(buffer);
                 println!("\t> Reply message size is: {}", message_size);
+                Ok(message_size)
+            }
+            Err(e) => {
+                panic!("Failed to receive data: {}", e);
+            }
+        };
+    }
 
-                let mut message_content = vec![0u8; message_size as usize];
-                match self.stream.read_exact(&mut message_content) {
-                    Ok(_) => {
-                        let response = from_utf8(&message_content).unwrap();
-                        println!("\t> Reply message is: {}", response);
-                    }
-                    Err(e) => {
-                        println!("\t> Failed to receive data: {}", e);
-                    }
-                }
+    fn read_message_content(&mut self, size: usize) {
+        let mut message_content = vec![0u8; size];
+        match self.stream.read_exact(&mut message_content) {
+            Ok(_) => {
+                let response = from_utf8(&message_content).unwrap();
+                println!("\t> Reply message is: {}", response);
             }
             Err(e) => {
                 println!("\t> Failed to receive data: {}", e);
