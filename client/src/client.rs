9ced28c8a68a0dbc0521648919615ccf85fcec29
henrimeng_predilife <henri.meng@predilife.com>
2022-07-02 16:40:53 +0200

    [ADD] Tcp Read
    "Communication Started with localhost:7878 ...
    	> Sent "Hello"
    	> Reply message size is: 25
    	> Reply message is: {"Welcome":{"version
    Communication Terminated."
    
    [TODO] use Loop to read the exact size of the message (issue: non-constant value)

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,10 +1,11 @@
+use std::char::from_u32;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
 use crate::message_type::MessageType;
 
 pub struct Client {
-    pub stream: TcpStream
+    pub stream: TcpStream,
 }
 
 impl Client {
@@ -15,16 +16,24 @@ impl Client {
         self.stream.write(msg.as_bytes()).unwrap();
         println!("\t> Sent {}", msg);
 
-        let mut data = [0 as u8; 4];
-        match self.stream.read_exact(&mut data) {
+        let mut buffer = [0 as u8; 4];
+        match self.stream.read_exact(&mut buffer) {
             Ok(_) => {
-                if &data == msg.as_bytes() {
-                    println!("\t> Reply is ok!");
-                } else {
-                    let text = from_utf8(&data).unwrap();
-                    println!("\t> Unexpected reply: {}", text);
+                let text = u32::from_be_bytes(buffer);
+                println!("\t> Reply message size is: {}", text);
+
+                // TODO
+                let mut data = [0 as u8; 20];
+                match self.stream.read_exact(&mut data) {
+                    Ok(_) => {
+                        let response = from_utf8(&data).unwrap();
+                        println!("\t> Reply message is: {}", response);
+                    }
+                    Err(e) => {
+                        println!("\t> Failed to receive data: {}", e);
+                    }
                 }
-            },
+            }
             Err(e) => {
                 println!("\t> Failed to receive data: {}", e);
             }
