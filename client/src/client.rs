eed235b146c1c8426548e327a2bb301e409d8c24
Ahmedine <krapulxx@gmail.com>
2022-07-06 12:56:40 +0200

    [Merge] test / dev

--- client/src/client.rs
+++ client/src/client.rs
@@ -24,7 +24,7 @@ impl Client {
         println!("\n\t> Sent {:?}", msg);
     }
 
-    fn read_message_size(&mut self) -> u32 {
+     fn read_message_size(&mut self) -> u32 {
         let mut buffer = [0 as u8; 4];
         self.stream.read_exact(&mut buffer).expect("Failed to read message size !");
         let message_size = u32::from_be_bytes(buffer);
@@ -32,7 +32,7 @@ impl Client {
         message_size
     }
 
-    fn read_message_content(&mut self, size: usize) -> MessageType {
+     fn read_message_content(&mut self, size: usize) -> MessageType {
         let mut message_content = vec![0u8; size];
         self.stream.read_exact(&mut message_content).expect("Failed to read message content !");
         let response = from_utf8(&message_content).unwrap();
@@ -40,4 +40,49 @@ impl Client {
         println!("\t> Reply message is: {:?}", &deserialized);
         deserialized
     }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::client::Client;
+    use crate::message_type::MessageType;
+    use std::net::TcpStream;
+
+    #[test]
+    fn test_watching_hello() {
+        let mut client = Client {
+            stream: TcpStream::connect("localhost:7878").expect("Couldn't connect to the server...")
+        };
+        let test = MessageType::Hello;
+        client.send(test);
+        let result = Client::watching(&mut client);
+        assert!(matches!(result, test));
+    }
+
+    #[test]
+    fn test_read_message_size() {
+        let mut client = Client {
+            stream: TcpStream::connect("localhost:7878").expect("Couldn't connect to the server...")
+        };
+        let test = MessageType::Hello;
+        client.send(test);
+
+        let result = Client::read_message_size(&mut client);
+        assert_eq!(result, 25);
+    }
+
+    // #[test]
+    // fn test_read_message_content() {
+    //     let mut client = Client {
+    //         stream: TcpStream::connect("localhost:7878").expect("Couldn't connect to the server...")
+    //     };
+    //
+    //     let test = MessageType::Hello;
+    //     let result = Client::read_message_content(&mut client, 2);
+    //     assert!(matches!(result, test))
+    // }
+
+
 }
